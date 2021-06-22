12830cd701234faac72751171638bb946d1659a8
rizalaji1st <rizalaji561@gmail.com>
2021-06-22 23:03:20 +0700

    Menambah fitur favorite

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
+import '../providers/products.dart';
+import 'package:provider/provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   // final String title;
-  
+
   // ProductDetailScreen(this.title);
   static const routeName = '/product-detail';
 
   @override
   Widget build(BuildContext context) {
     final route = ModalRoute.of(context);
-    if(route == null) return SizedBox.shrink();
+    if (route == null) return SizedBox.shrink();
     final productId = route.settings.arguments as String;
-    
+    final loadedProducts = Provider.of<Products>(
+      context,
+      listen: false,
+    ).findById(productId);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        title: Text(loadedProducts.title),
       ),
     );
   }
