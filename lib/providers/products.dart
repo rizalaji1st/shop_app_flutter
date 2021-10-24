6313c757f0ccca549cf8b7eab593abfaed8992d3
rizalaji1st <rizalaji561@gmail.com>
2021-10-24 23:31:46 +0700

    Saving the product

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import './product.dart';
 
-class Products with ChangeNotifier{
+class Products with ChangeNotifier {
   List<Product> _items = [
     Product(
       id: 'p1',
@@ -39,15 +39,15 @@ class Products with ChangeNotifier{
 
   // var _showFavoriteOnly = false;
 
-  List<Product> get items{
+  List<Product> get items {
     // if(_showFavoriteOnly){
     //  return _items.where((prodItem) => prodItem.isFavorite).toList();
     // }
     return [..._items];
   }
 
-  List<Product> get favoriteItems{
-      return _items.where((prodItem) => prodItem.isFavorite).toList();
+  List<Product> get favoriteItems {
+    return _items.where((prodItem) => prodItem.isFavorite).toList();
   }
 
   // void showFavoritesOnly(){
@@ -60,12 +60,19 @@ class Products with ChangeNotifier{
   //   notifyListeners();
   // }
 
-  Product findById(String id){
+  Product findById(String id) {
     return _items.firstWhere((prod) => prod.id == id);
   }
 
-  void addProduct(value){
-    _items.add(value);
+  void addProduct(Product product) {
+    final newProduct = Product(
+      id: DateTime.now().toString(),
+      title: product.title,
+      description: product.description,
+      price: product.price,
+      imageUrl: product.imageUrl,
+    );
+    _items.add(newProduct);
     notifyListeners();
   }
 }
--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../providers/product.dart';
+import '../providers/products.dart';
 
 class EditProductScreen extends StatefulWidget {
   static const routeName = '/edit-product';
@@ -37,10 +39,8 @@ class _EditProductScreenState extends State<EditProductScreen> {
     final isValid = _form.currentState!.validate();
     if (!isValid) return;
     _form.currentState!.save();
-    print(_editedProduct.title);
-    print(_editedProduct.price);
-    print(_editedProduct.description);
-    print(_editedProduct.imageUrl);
+    Provider.of<Products>(context, listen: false).addProduct(_editedProduct);
+    Navigator.of(context).pop();
   }
 
   @override
