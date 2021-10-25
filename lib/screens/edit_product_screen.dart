81aa1ed5e13b34a6a0c8282de869d3724c6d9d89
rizalaji1st <rizalaji561@gmail.com>
2021-10-25 19:16:23 +0700

    Menambah error handler

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -82,7 +82,23 @@ class _EditProductScreenState extends State<EditProductScreen> {
     } else {
       Provider.of<Products>(context, listen: false)
           .addProduct(_editedProduct)
-          .then((_) {
+          .catchError((error) {
+        return showDialog<Null>(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Text('An error occured'),
+            content: Text('Something went wrong'),
+            actions: [
+              TextButton(
+                child: Text('Ok'),
+                onPressed: () {
+                  Navigator.of(ctx).pop();
+                },
+              )
+            ],
+          ),
+        );
+      }).then((_) {
         setState(() {
           _isLoading = false;
         });
